46c973edf89443095ef1945a015363a10e4bf8ae
sajim-khan <shossain1427@gmail.com>
2023-01-27 20:31:47 +0600

    added

--- basic-array.js
+++ basic-array.js
@@ -1,11 +1,15 @@
-var array = [12,34,56,67,98,98];
-console.log(array[2]); //check index no
+// var array = [12,34,56,67,98,98];
+// console.log(array[2]); //check index no
 
-array[2] = 67; //value assign
+// array[2] = 67; //value assign
 
-console.log(array[2]);
+// console.log(array[2]);
 
-// find index of an element
-var positionIndex = array.indexOf(98);
-console.log(positionIndex);
+// // find index of an element
+// var positionIndex = array.indexOf(98);
+// console.log(positionIndex);
 
+let myArray = [1, 2, 3, 4, 5];
+console.log(myArray[2]);
+console.log(6 != 6);
+console.log(6 >= 6);
