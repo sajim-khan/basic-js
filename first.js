bd9f9e187a5659938c6e38f80c8e25ad3ba7703f
sajim-khan <shossain1427@gmail.com>
2023-01-27 16:31:04 +0600

    added practice problem solution

--- first.js
+++ first.js
@@ -1,6 +1,18 @@
-//practice -1
+//practice -1 added practice problem solution
 var givenMoney  = 1000;
 var cost = 700;
 var totalCost = givenMoney -cost;
 console.log("Output:",totalCost);
+
+//practice -2
+
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var score = mathematics + biology + chemistry + physics + bangla;
+var result = score / 5 ;
+result = result.toFixed(2)
+console.log(result);
 
