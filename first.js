d612bfadee76444429f5a7c328565af9474f75be
sajim-khan <shossain1427@gmail.com>
2023-01-27 16:40:01 +0600

    added practice problem solution

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
 //practice -1 added practice problem solution
 var givenMoney  = 1000;
 var cost = 700;
-var totalCost = givenMoney -cost;
+totalCost = givenMoney -cost;
 console.log("Output:",totalCost);
 
 //practice -2
@@ -12,13 +12,19 @@ var chemistry = 80;
 var physics = 35.45;
 var bangla = 99.50;
 var score = mathematics + biology + chemistry + physics + bangla;
-var result = score / 5 ;
+result = score / 5 ;
 result = result.toFixed(2)
-console.log(result);
+console.log("Output:",result);
 
 //practice -3
 
 var firstValue = 'I am going to be';
 var secondValue = 'an awesome web developer';
- var result = firstValue +' '+ secondValue;
- console.log(result);
+result = firstValue +' '+ secondValue;
+ console.log("Output:",result);
+
+//practice -4
+
+var value = 119;
+result = value % 5;
+console.log("Output:",result);
